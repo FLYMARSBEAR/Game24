b79ef1f994b211fd0f0c196d04b83dc8a25f4d3e
FLYMARSBEAR <chentao.thu@gmail.com>
2014-07-27 23:37:32 +0800

    Update index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,16 +1,40 @@
+var windowx,windowh;
 $(document).ready(function(){
+	windowx=window.screen.width;
+	windowh=window.screen.height;
+	//$("#index_body").css("left",(window.screen.width-1024)/2);
+	$("#index_body").css("left",windowx+"px").css("height",windowh+"px");
+	touchFunc($("#index_body").get(0),"start",haha);
+	touchFunc($("#index_body").get(0),"move",hehe);
 	
-	$("#index_body").css("left",(window.screen.width-1024)/2);
+	function haha(){
+		//alert("ss");
+	};
+	function hehe(){
+		//
+		return false;
+	};
+
 	
-	
+	/*document.body.onmousemove = function(event) {
+		return false;
+	    event = event || window.event;
+	    console.dir(event);	
+	};*/
 	
     // Global variables 
-    var ballX = 200; // Ball x position. 
-    var ballY = 200; // Ball y position. 
-    var ballDY = 0; // Change in ball y position.
-    var ballR=10;
+    var ballX = 20; // Ball x position. 
+    var ballY = 20; // Ball y position. 
+    var ballDX = 2; // Change in ball x position. 
+    var ballDY = 4; // Change in ball y position.
+    var ballR=5;
     var boardX = 500; // Board width. 
-    var boardY = 450; // Board height.
+    var boardY = 500; // Board height. 
+    var paddleDX = 0; // Change in board x position. 
+    var paddleX = 150; // Initial paddle location. 
+    var paddleH = 10; // Paddle height. 
+    //var paddleD = boardY - paddleH; // Paddle depth. 
+    var paddleW = 100; // Paddle width. 
     
     var hints_count=0;
 
@@ -30,25 +54,110 @@ $(document).ready(function(){
     ball.r.baseVal.value = ballR;
     ball.setAttribute("fill", "tomato");
     back.appendChild(ball);
-
-    var control=1;
-    drawGameSVG();
-    //stopGameSVG();
+    
+    var paddle = document.createElementNS("http://www.w3.org/2000/svg","rect");
+    paddle.setAttribute("x", 100);
+    paddle.setAttribute("y", 100);
+    paddle.setAttribute("width", paddleW);
+    paddle.setAttribute("height", paddleH);
+    paddle.setAttribute("fill", "navy");
+    back.appendChild(paddle);
+    
+    function gravity() {
+    	ballDY++;
+    }
+    
     function drawGameSVG() { 
       // Play the game until the ball stops. 
-    	gameLoop = setInterval(drawObstacle, 16); 
+      gameLoop = setInterval(drawBall, 16); 
+      //gravityLoop = setInterval(gravity, 96);
       // Add keyboard listener. 
-    	window.addEventListener('keydown', whatKey, true); 
+      window.addEventListener('keydown', whatKey, true); 
     } 
     
     function stopGameSVG() { 
         // Play the game until the ball stops. 
         clearInterval(gameLoop); 
+
         // Add keyboard listener. 
         window.removeEventListener('keydown', whatKey, true); 
     } 
+
+    function drawBall() { 
+    	
+    //document.getElementById("test").innerText+=new Date().getTime()+"\n\r";
+      // Change the ball location. 
+      ballX += ballDX; 
+      ballY += ballDY;
+      paddleX += paddleDX;
+      ball.setAttribute("cx", ballX); 
+      ball.setAttribute("cy", ballY); 
+      if(paddleX<=0){
+    	  paddleX=0;
+    	  paddleDX=0;
+      }
+      else if(paddleX>=boardX-paddleW){
+    	  paddleX=boardX-paddleW;
+    	  paddleDX=0;
+      }
+      
+      paddle.setAttribute("x", paddleX);
+   
+      
+
+
+      // Bounce on a left or right edge. 
+      if (ballX + ballDX > boardX - ballR || ballX + ballDX < ballR) ballDX = -ballDX; 
+
+      // If ball hits the top, bounce it.  
+      if (ballY + ballDY < ballR) ballDY = -ballDY; 
+      // If the ball hits the bottom, check see if it hits a paddle. 
+      else if (ballY + ballDY > boardY - ballR) { 
+        // If the ball hits the paddle, bounce it. 
+        if (ballX > paddleX && ballX < paddleX + paddleW) 
+        {
+        	ballDY = -ballDY;
+        	ballDX += paddleDX; 
+        	hints_count++;
+        	document.getElementById("count").innerText=hints_count;
+        }
+        // Otherwise the game is over. 
+        else { 
+          clearInterval(gameLoop); 
+          alert("Game over!"); 
+        } 
+      } 
+    } 
+
+
+
+    // Get key press. 
+
+
+    function whatKey(evt) { 
+
+      switch (evt.keyCode) { 
+        // Left arrow. 
+      case 37: 
+        paddleDX--; 
+        //if (paddleX < 0) paddleX = 0; 
+        //paddle.setAttribute("x", paddleX); 
+        break; 
+
+        // Right arrow. 
+      case 39: 
+    	paddleDX++;  
+        //if (paddleX > boardX - paddleW) paddleX = boardX - paddleW; 
+        //paddle.setAttribute("x", paddleX); 
+        break; 
+      } 
+    } 
     
-    $("#pause").click(function(){
+    var control=1;
+    drawGameSVG();
+    //stopGameSVG();
+    
+	$("#pause").click(function(){
 		if(control==1){
 			stopGameSVG();
 			document.getElementById("pause").innerText="RESTART";
@@ -59,136 +168,6 @@ $(document).ready(function(){
 			document.getElementById("pause").innerText="PAUSE";
 			control=1;
 		}
+		
 	});
-    
-    var Obstaclesup=new Array();
-    var Obstaclesdown=new Array();
-    var ObstacleW=80;
-    var basic=600;
-    var edge_fixed=50;
-    var holeH=150;
-    var space_times=3;
-    for(var i=0;i<1000;i++)
-    {
-    	var hole=parseInt(Math.random()*(boardY-2*edge_fixed-holeH)); 
-    	
-    	Obstaclesup[i]=document.createElementNS("http://www.w3.org/2000/svg","rect");
-    	Obstaclesup[i].setAttribute("x", space_times*ObstacleW*i+basic);
-    	Obstaclesup[i].setAttribute("y", 0);
-    	Obstaclesup[i].setAttribute("width", ObstacleW);
-    	Obstaclesup[i].setAttribute("height", edge_fixed+hole);
-    	Obstaclesup[i].setAttribute("fill", "navy");
-        back.appendChild(Obstaclesup[i]);
-        
-        Obstaclesdown[i]=document.createElementNS("http://www.w3.org/2000/svg","rect");
-    	Obstaclesdown[i].setAttribute("x", space_times*ObstacleW*i+basic);
-    	Obstaclesdown[i].setAttribute("y", edge_fixed+holeH+hole);
-    	Obstaclesdown[i].setAttribute("width", ObstacleW);
-    	Obstaclesdown[i].setAttribute("height", boardY-hole-edge_fixed-holeH);
-    	Obstaclesdown[i].setAttribute("fill", "navy");
-        back.appendChild(Obstaclesdown[i]);
-    }
-    
-    var timer=0;
-    var wave=1;
-    var next_wave=1;
-    function drawObstacle() { 
-    	
-    	if(timer%300==0)
-    	{
-    		wave=next_wave;
-    		var temp=Math.random()*2-1;
-    		if(temp>0){
-    			next_wave=wave+1;
-    			next_wave=Math.min(next_wave,1);
-    		}
-    		else{
-    			next_wave=wave-1;
-    			next_wave=Math.max(next_wave,-1);
-    		}
-    		
-    		switch (wave) { 
-    		case 1: 
-    			document.getElementById("wave_dir").innerText="DOWN";
-    			break; 
-    		case 0: 
-    			document.getElementById("wave_dir").innerText="NONE";
-    			ballDY=0;
-    			break; 
-    		case -1: 
-    			document.getElementById("wave_dir").innerText="UP";
-    			break;
-    		}
-    		
-    		switch (next_wave) { 
-    		case 1: 
-    			document.getElementById("next_wave_dir").innerText="DOWN";
-    			break; 
-    		case 0: 
-    			document.getElementById("next_wave_dir").innerText="NONE";
-    			ballDY=0;
-    			break; 
-    		case -1: 
-    			document.getElementById("next_wave_dir").innerText="UP";
-    			break;
-    		}
-    	}
-    	
-    	timer++;
-    	
-    	for(var i=0;i<1000;i++)
-        {
-    		Obstaclesup[i].setAttribute("x", space_times*ObstacleW*i+basic-timer);
-    		Obstaclesdown[i].setAttribute("x", space_times*ObstacleW*i+basic-timer);
-        }
-    	
-    	ballY+=ballDY;
-    	ball.setAttribute("cy",ballY);
-    	if(timer%10==0)
-    	{
-    		if(wave==0){
-    			ballDY-=ballDY/Math.max(Math.abs(ballDY),1);
-    		}
-    		else{
-    			ballDY+=wave;
-    		}	
-    	}
-    	
-    //document.getElementById("test").innerText+=new Date().getTime()+"\n\r";
-    } 
-
-
-
-    // Get key press. 
-
-    
-    var hgive=20;
-    var vgive=4;
-    function whatKey(evt) { 
-    	switch (evt.keyCode) { 
-    		case 38: 
-    			ballY-=hgive;
-    			ballDY-=vgive;
-    			if(ballDY<-4)
-    				ballDY=-4;
-    			break; 
-    		case 40: 
-    			ballY+=hgive;
-    			ballDY+=vgive;
-    			if(ballDY>4)
-    				ballDY=4;
-    			break; 
-    	}
-    }
-       /*  // Left arrow. 
-      case 37: 
-        paddleDX--; 
-        break; 
-
-        // Right arrow. 
-      case 39: 
-    	paddleDX++; 
-        break; 
-      */
-	
 });
