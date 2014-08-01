6e5edd41456a7d287d3b4b623c7ff263c2fc1197
FLYMARSBEAR <chentao.thu@gmail.com>
2014-08-01 19:30:48 +0800

    Update index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,173 +1,150 @@
 var windowx,windowh;
+var canvas,ctx,arr,then;
+var uncleimage,hpbarimage,hpimage,imagearr;
+var fistimage,clawimage,broomimage;
+var hp;
+var totaltime=0;
+var isStart=false;
+var beat1 =false;
+var beat2 =false;
+var beat3 =false;
+var beat4 =false;
+//var allstop=false;
 $(document).ready(function(){
 	windowx=window.screen.width;
 	windowh=window.screen.height;
-	//$("#index_body").css("left",(window.screen.width-1024)/2);
-	$("#index_body").css("left",windowx+"px").css("height",windowh+"px");
-	touchFunc($("#index_body").get(0),"start",haha);
+	$("#index_body").css("width",windowx+"px").css("height",windowh+"px");
+	
 	touchFunc($("#index_body").get(0),"move",hehe);
 	
-	function haha(){
-		//alert("ss");
-	};
-	function hehe(){
-		//
-		return false;
-	};
-
 	
-	/*document.body.onmousemove = function(event) {
-		return false;
-	    event = event || window.event;
-	    console.dir(event);	
-	};*/
+
+	// Create the canvas  
+	canvas = document.createElement("canvas");  
+	ctx = canvas.getContext("2d");  
+	canvas.width = 320;  
+	canvas.height = 400; 
+	document.body.appendChild(canvas);
+	touchFunc(canvas,"start",beat);
+
+	arr = new Array();
+	imagearr = new Array();
+	reset();
+	then = Date.now();
+	// The main game loop  
 	
-    // Global variables 
-    var ballX = 20; // Ball x position. 
-    var ballY = 20; // Ball y position. 
-    var ballDX = 2; // Change in ball x position. 
-    var ballDY = 4; // Change in ball y position.
-    var ballR=5;
-    var boardX = 500; // Board width. 
-    var boardY = 500; // Board height. 
-    var paddleDX = 0; // Change in board x position. 
-    var paddleX = 150; // Initial paddle location. 
-    var paddleH = 10; // Paddle height. 
-    //var paddleD = boardY - paddleH; // Paddle depth. 
-    var paddleW = 100; // Paddle width. 
-    
-    var hints_count=0;
-
-    // This function is called on page load. 
-
-    var bpard = document.createElementNS("http://www.w3.org/2000/svg","rect");
-    bpard.setAttribute("x", 1);
-    bpard.setAttribute("y", 1);
-    bpard.setAttribute("width", boardX-2);
-    bpard.setAttribute("height", boardY-2);
-    bpard.setAttribute("fill", "thistle");
-    back.appendChild(bpard);
-    
-    var ball = document.createElementNS("http://www.w3.org/2000/svg","circle");
-    ball.setAttribute("cx", ballX);
-    ball.setAttribute("cy", ballY);
-    ball.r.baseVal.value = ballR;
-    ball.setAttribute("fill", "tomato");
-    back.appendChild(ball);
-    
-    var paddle = document.createElementNS("http://www.w3.org/2000/svg","rect");
-    paddle.setAttribute("x", 100);
-    paddle.setAttribute("y", 100);
-    paddle.setAttribute("width", paddleW);
-    paddle.setAttribute("height", paddleH);
-    paddle.setAttribute("fill", "navy");
-    back.appendChild(paddle);
-    
-    function gravity() {
-    	ballDY++;
-    }
-    
-    function drawGameSVG() { 
-      // Play the game until the ball stops. 
-      gameLoop = setInterval(drawBall, 16); 
-      //gravityLoop = setInterval(gravity, 96);
-      // Add keyboard listener. 
-      window.addEventListener('keydown', whatKey, true); 
-    } 
-    
-    function stopGameSVG() { 
-        // Play the game until the ball stops. 
-        clearInterval(gameLoop); 
-
-        // Add keyboard listener. 
-        window.removeEventListener('keydown', whatKey, true); 
-    } 
-
-    function drawBall() { 
-    	
-    //document.getElementById("test").innerText+=new Date().getTime()+"\n\r";
-      // Change the ball location. 
-      ballX += ballDX; 
-      ballY += ballDY;
-      paddleX += paddleDX;
-      ball.setAttribute("cx", ballX); 
-      ball.setAttribute("cy", ballY); 
-      if(paddleX<=0){
-    	  paddleX=0;
-    	  paddleDX=0;
-      }
-      else if(paddleX>=boardX-paddleW){
-    	  paddleX=boardX-paddleW;
-    	  paddleDX=0;
-      }
-      
-      paddle.setAttribute("x", paddleX);
-   
-      
-
-
-      // Bounce on a left or right edge. 
-      if (ballX + ballDX > boardX - ballR || ballX + ballDX < ballR) ballDX = -ballDX; 
-
-      // If ball hits the top, bounce it.  
-      if (ballY + ballDY < ballR) ballDY = -ballDY; 
-      // If the ball hits the bottom, check see if it hits a paddle. 
-      else if (ballY + ballDY > boardY - ballR) { 
-        // If the ball hits the paddle, bounce it. 
-        if (ballX > paddleX && ballX < paddleX + paddleW) 
-        {
-        	ballDY = -ballDY;
-        	ballDX += paddleDX; 
-        	hints_count++;
-        	document.getElementById("count").innerText=hints_count;
-        }
-        // Otherwise the game is over. 
-        else { 
-          clearInterval(gameLoop); 
-          alert("Game over!"); 
-        } 
-      } 
-    } 
-
-
-
-    // Get key press. 
-
-
-    function whatKey(evt) { 
-
-      switch (evt.keyCode) { 
-        // Left arrow. 
-      case 37: 
-        paddleDX--; 
-        //if (paddleX < 0) paddleX = 0; 
-        //paddle.setAttribute("x", paddleX); 
-        break; 
-
-        // Right arrow. 
-      case 39: 
-    	paddleDX++;  
-        //if (paddleX > boardX - paddleW) paddleX = boardX - paddleW; 
-        //paddle.setAttribute("x", paddleX); 
-        break; 
-      } 
-    } 
-    
-    var control=1;
-    drawGameSVG();
-    //stopGameSVG();
-    
-	$("#pause").click(function(){
-		if(control==1){
-			stopGameSVG();
-			document.getElementById("pause").innerText="RESTART";
-			control=0;
-		}
-		else{
-			drawGameSVG();
-			document.getElementById("pause").innerText="PAUSE";
-			control=1;
-		}
-		
-	});
+	
+	// Let's play this game!  
+	setInterval(main, 1); // Execute as fast as possible  
+	
 });
+
+function hehe(){
+	return false;
+};
+
+function main(){  
+	if(isStart==false){
+		ctx.fillStyle = "rgb(0, 0, 0)";  
+	    ctx.font = "24px Microsoft YaHei";   
+	    ctx.fillText("消灭怪大叔", 60, 100);
+	    ctx.fillText("点击开始游戏", 60, 200);
+	}
+	else if(hp>0){
+		var now = Date.now();  
+	    var delta = now - then;  
+	    //console.log(delta);
+	    totaltime+=delta;
+	    update(delta / 1000);  
+	    render();  
+	  
+	    then = now;
+	}
+	else{
+		ctx.clearRect(0,0,windowx,windowh);
+		ctx.fillStyle = "rgb(0, 0, 0)";  
+	    ctx.font = "24px Microsoft YaHei";   
+	    ctx.fillText("你消灭了怪大叔", 60, 100);
+	    ctx.fillText("用了" + totaltime, 60, 130);
+	    //ctx.fillText("点击开始游戏", 60, 200);
+	}
+      
+};  
+//Reset the game when the player catches a monster  
+function reset() {  
+	for (var i=1;i<5;i++){
+		//arr[i] = parseInt(Math.random()*3)+1;
+		imagearr[i-1] = new Image();
+		imagearr[i-1].src = "images/btn"+ i +".png";
+	}
+	hp = 1000;
+	uncleimage = new Image();
+	uncleimage.src = "images/uncle.png";
+	hpimage = new Image();
+	hpimage.src = "images/hp.png";
+	hpbarimage = new Image();
+	hpbarimage.src = "images/hpbar.png";
+	fistimage = new Image();
+	fistimage.src = "images/fist.png";
+	clawimage = new Image();
+	clawimage.src = "images/claw.png";
+	broomimage = new Image();
+	broomimage.src = "images/broom" +".png";
+	
+};  
+//Update game objects  
+function update(modifier) { 
+	
+};
+//Draw everything  
+function render(){
+	ctx.clearRect(0,0,windowx,windowh);
+	
+	for (var i=0;i<3;i++){
+		ctx.drawImage(imagearr[i], 240-80*i, 320);
+	}
+	ctx.drawImage(uncleimage, 85, 140);
+	ctx.drawImage(hpbarimage, 0, 80);
+	ctx.drawImage(hpimage, 0, 0, 65+hp*235/1000, 55, 0, 80, 65+hp*235/1000, 55);
+	//ctx.drawImage(heroImage, hero.x, hero.y);
+	if(beat1==true){
+		ctx.drawImage(fistimage, 0, 180);
+	}
+	if(beat2==true){
+		ctx.drawImage(clawimage, 150, 150);
+	}
+	if(beat3==true){
+		ctx.drawImage(broomimage, 120, 50);
+	}
+	
+	ctx.fillStyle = "rgb(0, 0, 0)";  
+    ctx.font = "24px Helvetica";   
+    ctx.fillText("caught: " + totaltime, 32, 32);
+}; 
+
+//touchFunc(imagearr[3],"start",beat);
+function beat(){
+	if(isStart==false){
+		isStart=true;
+	}
+	if(isStart==true 
+		&&event.targetTouches[0].pageY>320&&event.targetTouches[0].pageY<400){
+		var temp = 3-parseInt(event.targetTouches[0].pageX/80);
+	
+		if(temp==0){
+			hp-=10;
+			beat1=true;
+			setTimeout("beat1=false;",150);
+		}
+		if(temp==1){
+			hp-=10;
+			beat2=true;
+			setTimeout("beat2=false;",150);
+		}
+		if(temp==2){
+			hp-=10;
+			beat3=true;
+			setTimeout("beat3=false;",150);
+		}
+	}
+};
